ce326d7e6eb97de4875015e90bcb0d0be861d754
Kevin Bustos <cowbustos@gmail.com>
2024-08-05 09:51:36 -0300

    guardando ultimos cambios

--- script.js
+++ script.js
@@ -3,46 +3,44 @@ const addButton = document.getElementById('addButton');
 const taskList = document.getElementById('taskList');
 const searchInput = document.getElementById('searchInput');
 
-addButton.addEventListener('click', addTask);
-taskInput.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-        addTask();
-    }
-});
-
-function addTask() {
-    const taskText = taskInput.value;
-
-    if (taskText.trim() !== '') {
-        const newTask = document.createElement('li');
-        newTask.textContent = taskText;
-
-        newTask.addEventListener('click', function() {
-            newTask.classList.toggle('completed');
-        });
-
-        taskList.appendChild(newTask);
-
-        taskInput.value = '';
-    }
-}
-
-taskList.addEventListener('click', function(event) {
-    if (event.target.tagName === 'LI') {
-        taskList.removeChild(event.target);
-    }
-});
-
-searchInput.addEventListener('input', function() {
-    const filter = searchInput.value.toLowerCase();
-    const tasks = taskList.getElementsByTagName('li');
-
-    Array.from(tasks).forEach(function(task) {
-        const taskText = task.textContent.toLowerCase();
-        if (taskText.includes(filter)) {
-            task.style.display = '';
-        } else {
-            task.style.display = 'none';
+    addButton.addEventListener('click', addTask);
+    taskInput.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            addTask();
         }
     });
-});
+
+    function addTask() {
+         const taskText = taskInput.value;
+
+        if (taskText.trim() !== '') {
+            const newTask = document.createElement('li');
+            newTask.textContent = taskText;
+
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Eliminar';
+            deleteButton.classList.add('delete-button');
+            deleteButton.addEventListener('click', function() {
+                taskList.removeChild(newTask);
+            });
+
+            newTask.appendChild(deleteButton);
+            taskList.appendChild(newTask);
+
+            taskInput.value = '';
+        }
+    }
+
+        searchInput.addEventListener('input', function() {
+            const filter = searchInput.value.toLowerCase();
+            const tasks = taskList.getElementsByTagName('li');
+
+            Array.from(tasks).forEach(function(task) {
+                const taskText = task.firstChild.textContent.toLowerCase();
+                if (taskText.includes(filter)) {
+                    task.style.display = '';
+                } else {
+                    task.style.display = 'none';
+                }
+            });
+        });
